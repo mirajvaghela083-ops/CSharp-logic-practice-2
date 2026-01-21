cf4d12caec7f3f0c55302f7e55b65937c76e8276
mirajvaghela083-ops <mirajvaghela083@gmail.com>
2026-01-21 15:15:33 +0530

    Add logic to print numbers from 1 to 10

---
+++ Logicbuildpractice/DoWhile1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+// Print all numbers from 1 to 10.
+namespace Logicbuildpractice
+{
+    internal class DoWhile1
+    {
+        internal static void printNum()
+        {
+            int i = 0;
+            do
+            {
+                i++;
+                Console.WriteLine(i);
+            } while (i < 10);
+        }
+    }
+}
