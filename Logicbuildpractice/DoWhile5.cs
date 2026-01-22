019d1d8189296b531e59933a545313b258bd6546
mirajvaghela083-ops <mirajvaghela083@gmail.com>
2026-01-22 17:14:40 +0530

    Count and print the number of digits in a given number

---
+++ Logicbuildpractice/DoWhile5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+// Count and print the number of digits in a given number
+namespace Logicbuildpractice
+{
+    internal class DoWhile5
+    {
+        internal static void count()
+        {
+            Console.WriteLine("the number is :");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int count = 0;
+
+            do
+            {
+                 num = num / 10;
+            count++;
+               
+            } while (num > 0);
+            Console.WriteLine(count);
+        }
+    }
+}
--- Logicbuildpractice/Program.cs
+++ Logicbuildpractice/Program.cs
@@ -9,6 +9,7 @@ class Program
         //DoWhile1.printNum();
         //DoWhile2.multi();
         //DoWhile3.sum();
-      //  DoWhile4.largestNum();
+        //  DoWhile4.largestNum();
+        DoWhile5.count();
     }
 }
