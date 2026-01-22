c558f629ade375ae4af8e7245bd9f643f6901625
mirajvaghela083-ops <mirajvaghela083@gmail.com>
2026-01-22 15:44:55 +0530

    Keep taking numbers until 0 is entered, then print the sum of all entered numbers

---
+++ Logicbuildpractice/DoWhile3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+// Keep taking numbers until 0 is entered, then print the sum of all entered numbers
+namespace Logicbuildpractice
+{
+    internal class DoWhile3
+    {
+        internal static void sum()
+        {
+            int number;
+            int sum = 0;
+            do
+            {
+                Console.WriteLine("The number is :");
+                number = Convert.ToInt32(Console.ReadLine());
+
+                sum = sum + number;
+
+
+            } while (number != 0 );
+
+            if(number == 0)
+            {
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
